66688ada34f89decd95eefbff912e27869b473a8
woojun-choi <wjsnail@cau.ac.kr>
2025-11-12 23:00:42 +0900

    feat : section01 mp4 update

--- app/_components/Section1.tsx
+++ app/_components/Section1.tsx
@@ -6,7 +6,7 @@ import { useEffect, useRef } from 'react';
 import mainTitle from "@/app/assets/main-title.png";
 import mainTxt from "@/app/assets/main-txt.png";
 import inqBtn from "@/app/assets/inquiry_btn.png";
-import hero_mp4 from "@/app/assets/hero_mp4.png";
+// import hero_mp4 from "@/app/assets/hero_mp4.png";
 
 export default function Section1() {
   const sectionRef = useRef<HTMLDivElement>(null);
@@ -31,21 +31,13 @@ export default function Section1() {
 
   return (
     <section className="w-full aspect-[3/4] md:aspect-video flex flex-col items-center justify-center relative after:absolute after:z-[-2] after:w-full after:h-full after:top-0 after:left-0 after:content-[''] after:bg-[#222222] after:bg-opacity-[0.64]">
-      {/* 배경 이미지 */}
-      <div className="absolute top-0 left-0 inset-0 z-0 flex items-center justify-center">
-        <Image
-          src={hero_mp4}
-          alt="Hero Background"
-          className="w-full h-auto object-contain"
-          priority
-          quality={100}
-        />
-        {/* 어두운 오버레이 (선택사항) */}
-        <div className="absolute inset-0 bg-black/30"></div>
-      </div>
+      <video autoPlay loop muted playsInline controls={false} 
+        className='absolute top-0 left-0 w-full h-full z-0 items-center object-cover'>
+          <source src="/assets/main.mp4" type="video/mp4" />
+        </video>
 
-      {/* 컨텐츠 */}
-      <div className="w-full px-6 max-w-[1548px] h-full hidden md:flex items-center justify-center mx-auto">
+      {/* PC 컨텐츠 */}
+      <div className="absolute hidden md:flex top-0 left-0 inset-0 z-0 flex items-center justify-center">
         <div className="flex flex-col items-center gap-[5vw] 2xl:gap-[80px] w-[65.2%]">
           <Image
             src={mainTitle}
@@ -78,7 +70,42 @@ export default function Section1() {
           </a>
         </div>
       </div>
-
+      
+      {/* MO 컨텐츠 */}
+      <div className="flex md:hidden flex-col gap-[9vw] w-full items-center mx-auto">
+        <div className="flex flex-col w-[84%] items-center pt-14">
+          <Image
+            src={mainTitle}
+            alt="대표님의 이름이 선택의 기준이 될수 있도록"
+            width={621}
+            height={277}
+            style={{ width: "55%", height: "auto" }}
+            className="el"
+          />
+          <Image
+            src={mainTxt}
+            alt="나만의 브랜드 블로그 마케팅으로"
+            width={544}
+            height={61}
+            style={{ width: "45%", height: "auto" }}
+            className="el"
+          />
+          <a
+          href="https://bit.ly/4qpx59b"
+          target="_blank"
+          className="el mt-[2vw] 2xl:mt-[30px]"
+          style={{ width: "22%", height: "auto" }}
+          >
+            <Image
+            src={inqBtn}
+            alt="문의하기 버튼"
+            width={319}
+            height={116}
+          />
+          </a>
+        </div>
+      </div>
+      
     </section>
   );
 }
