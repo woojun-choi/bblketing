405f660bf3b0059ebf2323042def8bf8f6145067
woojun-choi <wjsnail@cau.ac.kr>
2025-11-21 15:19:53 +0900

    build: git-hub web setting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+const nextConfig = {
+    output: 'export',
+    basePath: '/bblketing',
+}
 
 export default nextConfig;
