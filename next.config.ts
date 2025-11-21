d76750cada5b52de77c0a28d90fcd43d850d967d
woojun-choi <wjsnail@cau.ac.kr>
2025-11-21 16:41:26 +0900

    feat: bulid rollback

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
+const nextConfig: NextConfig = {
+  /* config options here */
+};
 
-const nextConfig = {
-    output: 'export',
-    basePath: '/bblketing',
-}
+// const nextConfig : NextConfig = {
+//     output: 'export',
+//     basePath: '/bblketing',
+// }
 
-const prefix =
-  process.env.NODE_ENV === 'production' ? 'https://woojun-choi.github.io/bblketing/' : ''
+// // const prefix =
+// //   process.env.NODE_ENV === 'production' ? 'https://woojun-choi.github.io/bblketing/' : ''
 
 export default nextConfig;
